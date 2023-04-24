c1f31a4d359b83212637b0e97e8a29c0d08f8956
Ignacio Velazquez <ignaciov92@gmail.com>
2023-04-24 17:21:30 -0300

    creación de repositorio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap'
+import { BrowserRouter, Routes } from "react-router-dom";
+import '../src/styles/App.css'
+function App() {
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
