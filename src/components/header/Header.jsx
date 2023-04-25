60b9c9fca17ef531838810ed5c1e41a050ea7a85
Nicolas Chaves <nicochaves92@gmail.com>
2023-04-25 14:05:35 -0300

    Creación de elemento Dropdowns

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,14 @@ import '../../styles/header.css'
 import React from 'react'
 import Navbar from '../navbar/Navbar'
 import SocialIcons from '../navbar/SocialIcons'
+import Dropdowns from '../navbar/Dropdowns'
 
 const Header = () => {
   return (
     <header className='d-flex flex-column'>
       <SocialIcons />
       <Navbar />
+      <Dropdowns />
     </header>
   )
 }
